09e8104d62164c6be7d99eee97c47eefbd2e672a
Alikhrik <siderdt@gmail.com>
2023-04-17 17:32:41 +0300

    Game Activity: Animation and Cells movement

--- app/src/main/java/step/learning/course/GameActivity.java
+++ app/src/main/java/step/learning/course/GameActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
     private final int N = 4;
     private final int[][] cells = new int[N][N];
+    private final int[][] saves = new int[N][N];
     private final TextView[][] tvCells = new TextView[N][N];
 
     private int score;
@@ -24,6 +28,9 @@ public class GameActivity extends AppCompatActivity {
     private TextView tvBestScore;
 
     private Random random;
+
+    private Animation spawnAnimation;
+    private Animation collapseAnimation;
 
     @SuppressLint({"DiscouragedApi", "MissingInflatedId"})
     @Override
@@ -36,6 +43,11 @@ public class GameActivity extends AppCompatActivity {
         tvScore.setText(getString(R.string.game_score, "69.6k"));
         tvBestScore.setText(getString(R.string.game_best_score, "69.6k"));
         random = new Random();
+
+        spawnAnimation = AnimationUtils.loadAnimation( this, R.anim.cell_spawn );
+        collapseAnimation = AnimationUtils.loadAnimation( this, R.anim.cell_collapse );
+        spawnAnimation.reset();
+        collapseAnimation.reset();
 
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
@@ -53,38 +65,63 @@ public class GameActivity extends AppCompatActivity {
                         new OnSwipeTouchListener(GameActivity.this) {
                             @Override
                             public void onSwipeRight() {
-                                Toast.makeText(
-                                                GameActivity.this,
-                                                "Right",
-                                                Toast.LENGTH_SHORT)
-                                        .show();
+                                if( canMoveRight() ) {
+                                    saveField();
+                                    moveRight();
+                                    spawnCell(1);
+                                    showField();
+                                } else {
+                                    Toast.makeText(
+                                                    GameActivity.this,
+                                                    "No Right Move",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
-
                             @Override
                             public void onSwipeLeft() {
-                                Toast.makeText(
-                                                GameActivity.this,
-                                                "Left",
-                                                Toast.LENGTH_SHORT)
-                                        .show();
+                                if( canMoveLeft() ) {
+                                    saveField();
+                                    moveLeft();
+                                    spawnCell(1);
+                                    showField();
+                                } else {
+                                    Toast.makeText(
+                                                    GameActivity.this,
+                                                    "No Left Move",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
-
                             @Override
                             public void onSwipeTop() {
-                                Toast.makeText(
-                                                GameActivity.this,
-                                                "Top",
-                                                Toast.LENGTH_SHORT)
-                                        .show();
+                                if( canMoveTop() ) {
+                                    saveField();
+                                    moveTop();
+                                    spawnCell(1);
+                                    showField();
+                                } else {
+                                    Toast.makeText(
+                                                    GameActivity.this,
+                                                    "No Top Move",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
-
                             @Override
                             public void onSwipeBottom() {
-                                Toast.makeText(
-                                                GameActivity.this,
-                                                "Bottom",
-                                                Toast.LENGTH_SHORT)
-                                        .show();
+                                if( canMoveBottom() ) {
+                                    saveField();
+                                    moveBottom();
+                                    spawnCell(1);
+                                    showField();
+                                } else {
+                                    Toast.makeText(
+                                                    GameActivity.this,
+                                                    "No Bottom Move",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
                         }
                 );
@@ -109,7 +146,65 @@ public class GameActivity extends AppCompatActivity {
                 cells[i][j] = 0;
             }
         }
+        score = 0;
         spawnCell( 2 );
+        showField();
+    }
+    private boolean canMoveRight() {
+        for ( int i = 0; i < N; i++ ) {
+            for ( int j = N-1; j > 0; j-- ) {
+                if (cells[i][j] == 0
+                        && cells[i][j - 1] != 0) {
+                    return true;
+                }
+                if( cells[i][j] == cells[i][j-1] && cells[i][j] != 0 ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean canMoveLeft() {
+        for ( int i = 0; i < N; i++ ) {
+            for ( int j = 0; j < N-1; j++ ) {
+                if (cells[i][j] == 0
+                        && cells[i][j + 1] != 0) {
+                    return true;
+                }
+                if( cells[i][j] == cells[i][j+1] && cells[i][j] != 0 ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean canMoveTop() {
+        for ( int i = 0; i < N; i++ ) {
+            for ( int j = 0; j < N-1; j++ ) {
+                if (cells[j][i] == 0
+                        && cells[j+1][i] != 0) {
+                    return true;
+                }
+                if( cells[j][i] == cells[j+1][i] && cells[j][i] != 0 ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean canMoveBottom() {
+        for ( int i = 0; i < N; i++ ) {
+            for ( int j = N-1; j > 0; j-- ) {
+                if (cells[j][i] == 0
+                        && cells[j-1][i] != 0) {
+                    return true;
+                }
+                if( cells[j][i] == cells[j-1][i] && cells[j][i] != 0 ) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @SuppressLint("DiscouragedApi")
@@ -162,10 +257,191 @@ public class GameActivity extends AppCompatActivity {
             cords.remove( randIndex );
             cnt--;
             count--;
+            tvCells[x][y].startAnimation( collapseAnimation );
         }
-
-        showField();
         return true;
+    }
+
+    private boolean moveRight() {
+        boolean isMoved = false;
+        for ( int i = 0; i < N; i++ ) {
+            boolean wasReplace;
+            do {
+                wasReplace = false;
+                for ( int j = N-1; j > 0; j-- ) {
+                    if( cells[i][j] == 0
+                     && cells[i][j-1] != 0 ) {
+                        cells[i][j] = cells[i][j-1];
+                        cells[i][j-1] = 0;
+                        wasReplace = true;
+                        isMoved = true;
+                    }
+                }
+            } while ( wasReplace );
+
+            // collapse
+            for ( int j = N-1; j > 0; j-- ) {
+                if( cells[i][j] == cells[i][j-1] && cells[i][j] != 0 ) {  // [2222]
+                    score +=  cells[i][j] + cells[i][j-1];
+                    cells[i][j] *= -2;                                    // [2224]
+                    cells[i][j-1] = 0;                                    // [2204] [2404] [2040]
+                    isMoved = true;
+                }
+            }
+
+            for ( int j = N-1; j > 0; j-- ) {
+                if( cells[i][j] == 0
+                 && cells[i][j-1] != 0 ) {
+                    cells[i][j] = cells[i][j-1];
+                    cells[i][j-1] = 0;
+                }
+            }
+
+            for ( int j = N-1; j > 0; j-- ) {
+                if( cells[i][j] < 0 ) {
+                    cells[i][j] = -cells[i][j];
+                    tvCells[i][j].startAnimation( collapseAnimation );
+                }
+            }
+        }
+        return isMoved;
+    }
+    private boolean moveLeft() {
+        boolean isMoved = false;
+        for ( int i = 0; i < N; i++ ) {
+            boolean wasReplace;
+            do {
+                wasReplace = false;
+                for ( int j = 0; j < N-1; j++ ) {
+                    if( cells[i][j] == 0
+                     && cells[i][j+1] != 0 ) {
+                        cells[i][j] = cells[i][j+1];
+                        cells[i][j+1] = 0;
+                        wasReplace = true;
+                        isMoved = true;
+                    }
+                }
+            } while ( wasReplace );
+
+            for ( int j = 0; j < N-1; j++ ) {
+                if( cells[i][j] == cells[i][j+1] // [2222] [0020]
+                 && cells[i][j] != 0 ) {
+                    score +=  cells[i][j] + cells[i][j+1];
+                    cells[i][j] *= -2;                                    // [2224]
+                    cells[i][j+1] = 0;                                    // [2204] [2404] [2040]
+                    isMoved = true;
+                }
+            }
+            for ( int j = 0; j < N-1; j++ ) {
+                if( cells[i][j] == 0
+                        && cells[i][j+1] != 0 ) {
+                    cells[i][j] = cells[i][j+1];
+                    cells[i][j+1] = 0;
+                }
+            }
+            for ( int j = 0; j < N-1; j++ ) {
+                if( cells[i][j] < 0 ) {
+                    cells[i][j] = -cells[i][j];
+                    tvCells[i][j].startAnimation( collapseAnimation );
+                }
+            }
+        }
+        return isMoved;
+    }
+    private boolean moveTop() {
+        boolean isMoved = false;
+        for ( int i = 0; i < N; i++ ) {
+            boolean wasReplace;
+            do {
+                wasReplace = false;
+                for ( int j = 0; j < N-1; j++ ) {
+                    if( cells[j][i] == 0
+                            && cells[j+1][i] != 0 ) {
+                        cells[j][i] = cells[j+1][i];
+                        cells[j+1][i] = 0;
+                        wasReplace = true;
+                        isMoved = true;
+                    }
+                }
+            } while ( wasReplace );
+
+            for ( int j = 0; j < N-1; j++ ) {
+                if( cells[j][i] == cells[j+1][i] // [2222] [0020]
+                        && cells[j][i] != 0 ) {
+                    score +=  cells[j][i] + cells[j+1][i];
+                    cells[j][i] *= -2;                                    // [2224]
+                    cells[j+1][i] = 0;                                    // [2204] [2404] [2040]
+                    isMoved = true;
+                }
+            }
+            for ( int j = 0; j < N-1; j++ ) {
+                if( cells[j][i] == 0
+                        && cells[j+1][i] != 0 ) {
+                    cells[j][i] = cells[j+1][i];
+                    cells[j+1][i] = 0;
+                }
+            }
+            for ( int j = 0; j < N-1; j++ ) {
+                if( cells[j][i] < 0 ) {
+                    cells[j][i] = -cells[j][i];
+                    tvCells[j][i].startAnimation( collapseAnimation );
+                }
+            }
+        }
+        return isMoved;
+    }
+    private boolean moveBottom() {
+        boolean isMoved = false;
+        for ( int i = 0; i < N; i++ ) {
+            boolean wasReplace;
+            do {
+                wasReplace = false;
+                for ( int j = N-1; j > 0; j-- ) {
+                    if( cells[j][i] == 0
+                     && cells[j-1][i] != 0 ) {
+                        cells[j][i] = cells[j-1][i];
+                        cells[j-1][i] = 0;
+                        wasReplace = true;
+                        isMoved = true;
+                    }
+                }
+            } while ( wasReplace );
+
+            for ( int j = N-1; j > 0; j-- ) {
+                if( cells[j][i] == cells[j-1][i] // [2222] [0020]
+                 && cells[j][i] != 0 ) {
+                    score +=  cells[j][i] + cells[j-1][i];
+                    cells[j][i] *= -2;                                    // [2224]
+                    cells[j-1][i] = 0;                                    // [2204] [2404] [2040]
+                    isMoved = true;
+                }
+            }
+            for ( int j = N-1; j > 0; j-- ) {
+                if( cells[j][i] == 0
+                 && cells[j-1][i] != 0 ) {
+                    cells[j][i] = cells[j-1][i];
+                    cells[j-1][i] = 0;
+                }
+            }
+            for ( int j = N-1; j > 0; j-- ) {
+                if( cells[j][i] < 0 ) {
+                    cells[j][i] = -cells[j][i];
+                    tvCells[j][i].startAnimation( collapseAnimation );
+                }
+            }
+        }
+        return isMoved;
+    }
+
+    private void saveField() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy( cells[i], 0, saves[i], 0, N );
+        }
+    }
+    private void undoMove() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy( saves[i], 0, cells[i], 0, N );
+        }
     }
 
     private class Cord {
